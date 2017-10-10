a319d2ea60fc269b0e0d17a6ac74797398172877
Wei Liu <284613412@qq.com>
2017-10-10 21:18:49 +0800

    Update test_image_classifier.py

--- test/test_image_classifier.py
+++ test/test_image_classifier.py
@@ -1,3 +1,7 @@
+'''
+Reference:
+https://lijiancheng0614.github.io/2017/06/29/2017_06_29_TensorFlow-Slim-image-classification/
+'''
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
